0a5761aa1ab2af6627aa29e2cdc97e773c9e1cba
radifar <m.radifar05@gmail.com>
2021-05-28 03:18:53 +0700

    restructure package into modules

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,7 +4,12 @@ A Python package for analyzing and visualizing molecular files. For MOLSSI works
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas, zen
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
+
+import molecool.io
 
 # Handle versioneer
 from ._version import get_versions
