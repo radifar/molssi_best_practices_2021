6b1d9e35f1e11a25edb35d53aa4400bf04b276ec
radifar <m.radifar05@gmail.com>
2021-05-29 00:26:08 +0700

    move mol mass test to new test module

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -50,13 +50,3 @@ def test_build_bond_list():
     bonds = molecool.build_bond_list(coordinates)
 
     assert len(bonds) == 4
-
-def test_molecular_mass():
-    symbols = ['C', 'H', 'H', 'H', 'H']
-
-    calculated_mass = molecool.calculate_molecular_mass(symbols)
-
-    actual_mass = 16.04
-
-    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
-
---
+++ molecool/tests/test_molecule.py
@@ -0,0 +1,16 @@
+"""
+Tests for the molecule module.
+"""
+
+import pytest
+import molecool
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+
