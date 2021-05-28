90a1eb69ca088c2e8506186e5fa2864f850ed80c
radifar <m.radifar05@gmail.com>
2021-05-29 00:54:19 +0700

    add test angle 60

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -5,6 +5,7 @@ Tests for the measure module.
 from molecool.measure import calculate_angle
 import molecool
 import numpy as np
+import pytest
 
 def test_calculate_distance():
     """Test that calculate_distance calculates what we expect"""
@@ -30,3 +31,17 @@ def test_calculate_angle():
     calculated_angle = molecool.calculate_angle(r1, r2, r3, True)
 
     assert expected_angle == calculated_angle
+
+def test_calculate_angle_60():
+    """Test that calculate_angle calculates what we expect"""
+
+    r1 = np.array([0,0,-1])
+    r2 = np.array([0,1,0])
+    r3 = np.array([1,0,0])
+
+    expected_angle = 60
+
+    calculated_angle = molecool.calculate_angle(r1, r2, r3, True)
+
+    assert pytest.approx(expected_angle) == calculated_angle
+
